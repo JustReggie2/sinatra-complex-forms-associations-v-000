3f422307890f1d191cd27808795bd8de08f4c231
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-12-28 02:45:31 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,11 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    binding.pry
+    @pet = Pet.create(params[:pet])
+    if !params["owner_name"].empty?
+      @owner = Owner.create(params["owner_name"])
+      @pet[:owner_id] = @owner[:id]
+    end
     redirect to "pets/#{@pet.id}"
   end
 
